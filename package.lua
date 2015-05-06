1dce3203c9688957343e9df57e83ab2d3bfa0e55
Nick Hildebrant <nick.hildebrant@gmail.com>
2015-05-06 10:36:43 +0200

    mv src and bump

--- package.lua
+++ package.lua
@@ -1,19 +1,16 @@
 return {
   name = "nihildeb/sqlite3",
-  version = "0.1.0",
+  version = "1.0.0",
   homepage = "https://github.com/nihildeb/lit-sqlite3",
   dependencies = {
-    "creationix/ffi-loader@1.0.0",
     "luvit/require@1.1.0",
     "luvit/pretty-print@1.0.1",
-    "luvit/utils@1.0.0",
+    "creationix/ffi-loader@1.0.0",
   },
   files = {
     "*.lua",
     "*.h",
-    "!sqlite-src",
     "!build",
-    "!test",
     "$OS-$ARCH/*",
   }
 }
