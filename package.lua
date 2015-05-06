2ebae42f427e99e9b1a3315ddbdbd2809fc84327
Nick Hildebrant <nick.hildebrant@gmail.com>
2015-05-06 10:34:11 +0200

    tests moved to samples project

--- package.lua
+++ package.lua
@@ -3,7 +3,10 @@ return {
   version = "0.1.0",
   homepage = "https://github.com/nihildeb/lit-sqlite3",
   dependencies = {
-    "creationix/ffi-loader@1.0.0"
+    "creationix/ffi-loader@1.0.0",
+    "luvit/require@1.1.0",
+    "luvit/pretty-print@1.0.1",
+    "luvit/utils@1.0.0",
   },
   files = {
     "*.lua",
